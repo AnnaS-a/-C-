a652849972236336b7ffefc581b85207cee30b76
anna <slukin1@yandex.ru>
2022-08-09 20:05:08 +0700

    tesk8

---
+++ Task8/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая будет принимать на вход число и выводить, является ли это число 7 и 23
+// 69 -> не кратно, 
+// 161 -> кратно
+
+Console.WriteLine("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 7 == 0 && number % 23 == 0)
+{
+     Console.WriteLine($"Число {number} кратно числам 7 и 23");
+}
+else Console.WriteLine($"Число {number} не кратно 7 и 23");
