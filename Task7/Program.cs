3d3f3d69fef6c856f4c3797d12eac8bb6e45f94c
anna <slukin1@yandex.ru>
2022-08-09 19:52:49 +0700

    tesk7

---
+++ Task7/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая выводит трёхзначное число и удаляет вторую цифру этого числа.
+// 456 -> 46
+
+int number = new Random().Next(100, 1000);
+int RemoveSecond(int num)
+{
+    int number1 = num / 100;
+    int number2 = num % 10;
+    int nov = number1 * 10 + number2;
+    return nov;
+}
+int result = RemoveSecond(number);
+Console.WriteLine($"{number} -> {result}");
+
+
