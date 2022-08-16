feb55b3a24a9785e41afa43a9afaf8684b4518d2
anna <slukin1@yandex.ru>
2022-08-16 09:41:39 +0700

    tesk10

---
+++ Task10/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая по заданному номеру четверти показывает диапозон возможных координат точек в этой четверти (Х и У)
+
+
+Console.WriteLine("Введите номер четверти ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+string Coordinat(int number)
+{
+    if (number == 1) return "Диапозон координат х > 0, y > 0";
+    if (number == 2) return "Диапозон координат х < 0, y > 0";
+    if (number == 3) return "Диапозон координат х < 0, y < 0";
+    if (number == 4) return "Диапозон координат х > 0, y < 0";
+    return "Введен некорректный номер четверти";
+}
+
+string result = Coordinat(num);
+Console.WriteLine(result);
