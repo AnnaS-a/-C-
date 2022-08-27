6d288b27dfa3ac306c7a2a267eee92af282826aa
anna <slukin1@yandex.ru>
2022-08-27 09:53:44 +0700

    tesk29

---
+++ Task29/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 49: Задайте двумерный массив. Видимые элементы, у
+// оба используемых индекса чётные, и заменители этих элементов на их квадраты.
+
+Console.Clear();
+int[,] CreateMtrixRndInt(int row, int col, int min, int max)
+{
+    int[,] matrix = new int[row, col];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(min, max);
+        }
+    }
+    return matrix;
+}
+
+void ReplaceEvenElement(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i += 2)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j += 2)
+        {
+
+            matrix[i, j] = matrix[i, j] * matrix[i, j];
+        }
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) System.Console.Write($"{matrix[i, j],4},");
+            else Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+int[,] array2d = CreateMtrixRndInt(3, 4, -9, 9);
+PrintMatrix(array2d);
+
+Console.WriteLine();
+
+ReplaceEvenElement(array2d);
+PrintMatrix(array2d);
